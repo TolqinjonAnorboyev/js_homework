c38139c2a7ef80cbb4e3711c7ce45861b79c5081
Anorboyev Tolqinjon <anorboyevtolqinjon@gmail.com>
2025-04-19 09:09:11 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Integer1
+let s;
+let a = 2;
+let b = 3;
+s = Math.sqrt(Math.sqrt((a**3 - b**3) + (a**3 + b**3)));
+console.log(s);
+
+// Integer2
+let z;
+let x = 8;
+let y = 5;
+z = Math.sqrt(Math.sqrt((x**3 - y**3) + (x**3 + y**3)));
+console.log(z);
