c7100c5e721e9d3dcd4e82a3e51ce2eea8c0f00e
Anorboyev Tolqinjon <anorboyevtolqinjon@gmail.com>
2025-04-24 10:47:58 +0500

    for questions added

---
+++ f1.js
@@ -0,0 +1,4 @@
+// If1 1- usul
+let a = 5;
+let b = 8;
+let c = 1;
--- math.js
+++ math.js
@@ -66,4 +66,91 @@ console.log(x);
 let x;
 let a = 0.5;
 x = Math.cos(a);
-console.log(x);*/
+console.log(x);
+
+//Integer11 
+let a = Number(prompt("Qiymat kiriting:"));
+let x1 = a % 10;
+let x2 = (a - x1) / 10;
+console.log("Ikki xonali son yig'indisi", x1+x2);
+console.log("Ikki xonali son ko'paytmasi", x1*x2);
+
+// Integer12
+let n = 12;
+let s;
+s = 2 * Math.pow( n + 3, 2);
+console.log(s);
+
+// Integer13
+
+let n = 5;
+let s;
+s = Math.pow( ((3 + n) / 2), 2);
+console.log(s);
+
+// Integer14
+
+let n = 4;
+let x = 2;
+let s;
+s = Math.pow(n, x) + Math.pow(6, x);
+console.log(s);
+
+// Integer15
+
+let x1 = -8;
+let x2 = -56;
+let x3 = -45;
+let a1 = Math.abs(x2);
+let a2 = Math.abs(x1);
+let a3 = Math.abs(x3);
+console.log(a1);
+console.log(a2);
+console.log(a3);
+
+// Integer 16
+
+let m1 = 3.456;
+let m2 = 5.678;
+let m3 = 7.5;
+let s1;
+let s2;
+let s3;
+s1 = Math.round(m1 * 100) / 100;
+s2 = Math.round(m2 * 100) / 100;
+s3 = Math.round(m3 * 100) / 100;
+console.log(s1);
+console.log(s2);
+console.log(s3); 
+
+// Integer 17
+
+let x = 2;
+let y = 3;
+let s;
+s = Math.pow(x, 4) + 5 * Math.pow(x, 2) + Math.pow(x, 3) * y;
+console.log(s); 
+
+// Integer 18
+
+let x = 2;
+let y = 3;
+let s;
+s = 6 * Math.pow(x, 3) * Math.pow (y, 5) + 4 * Math.pow(x, 4) * Math.pow(y, 3) - 24 * x * y;
+console.log(s); 
+
+// Integer 19 
+
+let a = Number(prompt("Qiymat kiriting:"));
+let x1 = a % 10;
+let x2 = ((a / 10) % 10) | 0;
+let x3 = (a / 100) | 0;
+console.log("Uch xonali son yig'indisi", x1+x2+x3);
+console.log("Uch xonali son ko'paytmasi", x1*x2*x3); 
+
+// Integer 20
+
+let s;
+s = Math.abs(5 / 3 - 13 / 4);
+console.log(s); */
+
